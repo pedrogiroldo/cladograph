6c25c3ffc34d5b9d7568a0a527e1d0ffcf648f80
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-06-16 11:16:47 -0300

    primeiro script que funcionou

--- apps/v1/main.py
+++ apps/v1/main.py
@@ -1,8 +1,26 @@
 import subprocess
 
-subprocess.check_call(['pip', 'install', '-r', 'requirements.txt'])
+subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
 
+import tkinter as tk
 from ete3 import Tree
 
-t = Tree
-print('a')
+t = Tree("minha_arvore.nw")
+
+def exibir_arvore():
+    t.show()
+
+
+def salvar_arvore():
+    t.render("cladograma.png", w=4000, units="px")
+
+
+root = tk.Tk()
+root.title("Visualizador de Árvore")
+
+btn_exibir = tk.Button(root, text="Exibir Árvore", command=exibir_arvore)
+btn_salvar = tk.Button(root, text="Salvar Árvore", command=salvar_arvore)
+btn_exibir.pack()
+btn_salvar.pack()
+
+root.mainloop()
