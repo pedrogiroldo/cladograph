be260a4ddd555d7e8a318103c0470973838eb870
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-11-02 16:16:13 -0300

    mudar noe do botao para adicionar/editar carac. Dependendo do valor do session storage, o botão pode aparecer como add ou editar

--- apps/electron-deprecated/src/renderer/pages/home/Home.tsx
+++ apps/electron-deprecated/src/renderer/pages/home/Home.tsx
@@ -1,14 +1,29 @@
 /* eslint-disable react/jsx-no-bind */
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import { Button, ButtonGroup, Grid, TextField } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Button, ButtonGroup, TextField } from '@mui/material';
 import Tree from '../../components/Tree/Tree';
 import './style.css';
+import { Traits } from '../../../models/traitsTypes';
+import { getTraits } from '../../../scripts/cacheManager/traitsCRUD';
 
 const defaultNewick =
   '(((EELA:0.150276,CONGERA:0.213019):0.230956,(EELB:0.263487,CONGERB:0.202633):0.246917):0.094785,((CAVEFISH:0.451027,(GOLDFISH:0.340495,ZEBRAFISH:0.390163):0.220565):0.067778,((((((NSAM:0.008113,NARG:0.014065):0.052991,SPUN:0.061003,(SMIC:0.027806,SDIA:0.015298,SXAN:0.046873):0.046977):0.009822,(NAUR:0.081298,(SSPI:0.023876,STIE:0.013652):0.058179):0.091775):0.073346,(MVIO:0.012271,MBER:0.039798):0.178835):0.147992,((BFNKILLIFISH:0.317455,(ONIL:0.029217,XCAU:0.084388):0.201166):0.055908,THORNYHEAD:0.252481):0.061905):0.157214,LAMPFISH:0.717196,((SCABBARDA:0.189684,SCABBARDB:0.362015):0.282263,((VIPERFISH:0.318217,BLACKDRAGON:0.109912):0.123642,LOOSEJAW:0.3971):0.287152):0.140663):0.206729):0.222485,(COELACANTH:0.558103,((CLAWEDFROG:0.441842,SALAMANDER:0.299607):0.135307,((CHAMELEON:0.771665,((PIGEON:0.150909,CHICKEN:0.172733):0.082163,ZEBRAFINCH:0.099172):0.272338):0.014055,((BOVINE:0.167569,DOLPHIN:0.15745):0.104783,ELEPHANT:0.166557):0.367205):0.050892):0.114731):0.295021):0;';
 
 export default function Home() {
+  const [alreadyAddedTraits, setAlreadyAddedTraits] = useState(false);
+
+  useEffect(() => {
+    const cachedTraits: Traits | undefined = getTraits();
+    if (cachedTraits === undefined) {
+      setAlreadyAddedTraits(false);
+    } else if (!cachedTraits[0]) {
+      setAlreadyAddedTraits(false);
+    } else if (cachedTraits[0]) {
+      setAlreadyAddedTraits(true);
+    }
+  }, [alreadyAddedTraits]);
+
   const navigate = useNavigate();
 
   const [nwkInputValue, setNwkInputValue] = useState('');
@@ -52,17 +67,23 @@ export default function Home() {
         </div>
         <div className="comparatorArea">
           <h1 id="comparatorTitle">Comparador</h1>
-          <ButtonGroup variant="outlined" size="medium">
-            <Button onClick={() => navigate('/addTraits')}>
-              Adicionar características
-            </Button>
+          <ButtonGroup variant="outlined" size="large" orientation="vertical">
+            {alreadyAddedTraits === true ? (
+              <Button onClick={() => navigate('/addTraits')}>
+                Editar características
+              </Button>
+            ) : (
+              <Button onClick={() => navigate('/addTraits')}>
+                Adicionar características
+              </Button>
+            )}
             <Button onClick={() => navigate('/addExternalGroup')}>
               Adicionar grupo ext.
             </Button>
             <Button>Adicionar descendente</Button>
             <Button>Gerar árvore</Button>
           </ButtonGroup>
-          <Grid container>
+          {/* <Grid container>
             <Grid item xs={6}>
               <div className="comparasionInfoButtons">
                 <Button
@@ -90,8 +111,8 @@ export default function Home() {
                   Visualizar descendentes
                 </Button>
               </div>
-            </Grid>
-            <Grid item xs={6}>
+            </Grid> */}
+          {/* <Grid item xs={6}>
               <div className="comparasionInfoContainer">
                 <Grid container>
                   <Grid item xs={6}>
@@ -109,7 +130,7 @@ export default function Home() {
                 </Grid>
               </div>
             </Grid>
-          </Grid>
+          </Grid> */}
         </div>
       </div>
     </div>
