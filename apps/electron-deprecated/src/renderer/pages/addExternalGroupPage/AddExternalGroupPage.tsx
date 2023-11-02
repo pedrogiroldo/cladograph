22c23215da48a39f9d5f5a31b67fafcd4228b8ba
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-11-02 17:20:52 -0300

    AddGroupTraitsPage

--- apps/electron-deprecated/src/renderer/pages/addExternalGroupPage/AddExternalGroupPage.tsx
+++ apps/electron-deprecated/src/renderer/pages/addExternalGroupPage/AddExternalGroupPage.tsx
@@ -2,27 +2,54 @@ import { useEffect, useState } from 'react';
 import { FormControlLabel, FormGroup } from '@mui/material';
 import BpCheckbox from '../../components/BpCheckbox/BpCheckbox';
 import Navbar from '../../components/Navbar/Navbar';
-import { main } from './styles';
+import { actionArea, externalGroupTraits, main } from './styles';
 import { Traits } from '../../../models/traitsTypes';
 import { getTraits } from '../../../scripts/cacheManager/traitsCRUD';
+import { ExternalGroup } from '../../../models/externalGroupTypes';
 
 export default function AddExternalGroupPage() {
   const [traits, setTraits] = useState<Traits>([]);
+  const [selectedTraits, setSelectedTraits] = useState<ExternalGroup>({});
 
   useEffect(() => {
     const cachedTraits: Traits | undefined = getTraits();
     if (cachedTraits === undefined) return;
+    const initialExternalGroup: ExternalGroup = {};
+    cachedTraits.forEach((trait) => {
+      initialExternalGroup[trait] = false; // Initialize all traits with false value
+    });
     setTraits(cachedTraits);
+    setSelectedTraits(initialExternalGroup);
   }, []);
+
+  const handleTraitChange = (trait: string) => {
+    setSelectedTraits((prevState) => ({
+      ...prevState,
+      [trait]: !prevState[trait], // Toggles the trait value
+    }));
+  };
 
   return (
     <div style={main}>
       <Navbar />
-      <FormGroup>
-        {traits.map((trait) => (
-          <FormControlLabel control={<BpCheckbox />} label={trait} />
-        ))}
-      </FormGroup>
+      <div style={actionArea}>
+        <h1 style={externalGroupTraits}>Características do Grupo Externo:</h1>
+        <FormGroup>
+          {traits.map((trait) => (
+            <FormControlLabel
+              key={trait}
+              control={
+                <BpCheckbox
+                  checked={!!selectedTraits[trait]} // Checking if the trait exists in selectedTraits
+                  onChange={() => handleTraitChange(trait)}
+                />
+              }
+              label={trait}
+            />
+          ))}
+        </FormGroup>
+        {/* <Button onClick={() => console.log(selectedTraits)}>ver</Button> */}
+      </div>
     </div>
   );
 }
--- apps/electron-deprecated/src/renderer/pages/addExternalGroupPage/styles.ts
+++ apps/electron-deprecated/src/renderer/pages/addExternalGroupPage/styles.ts
@@ -3,6 +3,13 @@ import CSS from 'csstype';
 export const main: CSS.Properties = {
   width: '100%',
   height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
 };
 
-export const paradedarerrobosta = [];
+export const actionArea: CSS.Properties = {};
+
+export const externalGroupTraits: CSS.Properties = {
+  color: '#1976D2',
+};
