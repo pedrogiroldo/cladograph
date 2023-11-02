0cf28e6def09a9afd8f63575c51edf1fba52dd7e
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-11-02 13:25:35 -0300

    criação do componente de checkbox

--- apps/electron-deprecated/src/renderer/pages/addExternalGroupPage/AddExternalGroupPage.tsx
+++ apps/electron-deprecated/src/renderer/pages/addExternalGroupPage/AddExternalGroupPage.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
+import { FormControlLabel, FormGroup } from '@mui/material';
+import BpCheckbox from '../../components/BpCheckbox/BpCheckbox';
 import Navbar from '../../components/Navbar/Navbar';
 import { main } from './styles';
+import { Traits } from '../../../models/traitsTypes';
+import { getTraits } from '../../../scripts/cacheManager/traitsCRUD';
 
 export default function AddExternalGroupPage() {
+  const [traits, setTraits] = useState<Traits>([]);
+
+  useEffect(() => {
+    const cachedTraits: Traits | undefined = getTraits();
+    if (cachedTraits === undefined) return;
+    setTraits(cachedTraits);
+  }, []);
+
   return (
     <div style={main}>
       <Navbar />
+      <FormGroup>
+        {traits.map((trait) => (
+          <FormControlLabel control={<BpCheckbox />} label={trait} />
+        ))}
+      </FormGroup>
     </div>
   );
 }
