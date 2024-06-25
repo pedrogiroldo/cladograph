2d1a287ebe275353988ada81cb393867b24722c6
pedrogiroldo <pedrogiroldo40@gmail.com>
2024-06-24 23:08:52 -0300

    start db

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PhylogeneticTreeScriptsModule } from './modules/phylogenetic-tree-scripts/phylogenetic-tree-scripts.module';
 import { PrismaService } from './databse/prisma/prisma.service';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PhylogeneticTreeScriptsModule],
+  imports: [PhylogeneticTreeScriptsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
---
+++ apps/backend/src/modules/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/databse/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private prismaService: PrismaService) { }
+
+  create(data: Prisma.UserCreateArgs) {
+    return this.prismaService.user.create(data);
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
