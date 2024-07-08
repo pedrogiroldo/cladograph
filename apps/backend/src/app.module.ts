070d5231fa4cc5f992e6e60fca2d6abcaaa60d16
pedrogiroldo <pedrogiroldo40@gmail.com>
2024-07-08 20:20:02 -0300

    app module solve

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,12 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PhylogeneticTreeScriptsModule } from './modules/phylogenetic-tree-scripts/phylogenetic-tree-scripts.module';
-<<<<<<< HEAD
-import { PrismaService } from './database/prisma/prisma.service';
-=======
 import { UsersModule } from './modules/users/users.module';
-import { PrismaModule } from './databse/prisma/prisma.module';
->>>>>>> feature/create-db
+import { PrismaModule } from './database/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule, PhylogeneticTreeScriptsModule, UsersModule],
